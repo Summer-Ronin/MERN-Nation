9d4620cd0ebe7aae7558b5e3c9291be7342dd03c
NhutNguyen236 <nhutnguyenf330@gmail.com>
2021-08-14 23:58:50 +0700

    :zap: turn route users to post ready to validate

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
 /**
- * @route GET api/users
- * @desc Test route
+ * @route POST api/users
+ * @desc register new user
  * @access Public
  */
-router.get("/", (req, res) => {
-	res.send("User route");
+router.post("/", (req, res) => {
+	console.log(req.body)
 });
 
-module.exports = router
+module.exports = router;
