cbcf9c732ddd4fa5d24f86985079f70a599d9f3b
NhutNguyen236 <nhutnguyenf330@gmail.com>
2021-08-15 12:31:49 +0700

    :zap: add basic express-validator validation on users

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,8 +7,24 @@ const { check, validationResult } = require("express-validator");
  * @desc register new user
  * @access Public
  */
-router.post("/", (req, res) => {
-	console.log(req.body)
-});
+router.post(
+	"/",
+	[
+		check("name", "Name is required").not().isEmpty(),
+		check("email", "Please enter email").isEmail(),
+		check(
+			"password",
+			"Please enter a password with 6 or more characters"
+		).isLength({ min: 6 }),
+	],
+	(req, res) => {
+		const validator_errors = validationResult(req);
+
+		// check out errors
+		if (!validator_errors.isEmpty()) {
+			return res.status(400).json({ errors: validator_errors.array() });
+		}
+	}
+);
 
 module.exports = router;
